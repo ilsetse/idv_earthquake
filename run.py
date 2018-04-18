78c10e1f04044d5eb3a28c0ed93006bd9d3a1ff5
ilsetse <ilsetse@gmail.com>
2018-04-18 12:15:15 +0300

    changed port

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
 
   app.jinja_env.auto_reload = True
   app.config['TEMPLATES_AUTO_RELOAD'] = True
-  app.run(host='localhost', port=8080, debug=True)
+  app.run(host='localhost', port=8000, debug=True)
