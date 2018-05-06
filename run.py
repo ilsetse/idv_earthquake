ef29868f92f946ca43a527258ec87ce5d91f8791
ilsetse <ilsetse@gmail.com>
2018-05-06 19:06:03 +0300

    update config files

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
+import os 
 from src import app
 import logging
 
+
+
 if __name__ == '__main__':
-  # API AUTH
-  #app.config.from_object('config.DevelopmentConfig')
   logger = logging.getLogger(__name__)
   logger.setLevel(logging.DEBUG)
 
   app.jinja_env.auto_reload = True
   app.config['TEMPLATES_AUTO_RELOAD'] = True
-  app.run(host='localhost', port=8000, debug=True)
+
+  #app.run(host='localhost', port=8000, debug=True)
+  port = int(os.environ.get('PORT', 8000)) 
+  app.run(host='0.0.0.0', port=port)
